d8fa1916128cd05fd784b84c5dc7fef90af0f9ad
RoKoga <koga.rod@gmail.com>
2019-03-29 15:16:05 -0300

    Add error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,6 +1,28 @@
 import { withNamespaces } from "../i18n";
+import { Head } from "../components";
 
-const Error = ({ t }) => <h1>{t("404")}</h1>;
+const Error = ({ t }) => (
+  <React.Fragment>
+    <Head title="XGH Certification - 404" />
+    <div className="content">
+      <h1>{t("404")}</h1>
+      <p>{t("explain")}</p>
+      {/* <p>{t("show-gif")}</p> */}
+      <style jsx>
+        {`
+          .content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100vw;
+            height: 100vh;
+          }
+        `}
+      </style>
+    </div>
+  </React.Fragment>
+);
 
 Error.getInitialProps = async () => {
   return {
