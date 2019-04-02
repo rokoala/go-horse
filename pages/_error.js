5f5c31b3eaa996830c780344a1cc4ef6a5e15d6f
rokoga <rokoga@gmail.com>
2019-04-02 14:19:49 -0300

    changes on error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,34 +1,41 @@
 import { Head } from "../components";
-import { useTranslation } from "react-i18next";
+import { withTranslation } from "react-i18next";
 
-export default () => {
-  const { t } = useTranslation("error");
+class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode };
+  }
+  render() {
+    const { t } = this.props;
+    return (
+      <React.Fragment>
+        <Head title="XGH Certification - 404" />
+        <div className="content">
+          <h1>
+            <a href="/">eXtreme GoHorse Process Certification</a>
+          </h1>
+          <h1>{t("404")}</h1>
+          {/* <p>{t("explain")}</p> */}
+          <img src="/static/images/construction.gif" />
+          <p>{t("show-gif")}</p>
+          <img src="/static/images/baby.gif" />
+          <style jsx>
+            {`
+              .content {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                width: 100vw;
+                height: 100vh;
+              }
+            `}
+          </style>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
-  return (
-    <React.Fragment>
-      <Head title="XGH Certification - 404" />
-      <div className="content">
-        <h1>
-          <a href="/">eXtreme GoHorse Process Certification</a>
-        </h1>
-        <h1>{t("404")}</h1>
-        {/* <p>{t("explain")}</p> */}
-        <img src="/static/images/construction.gif" />
-        <p>{t("show-gif")}</p>
-        <img src="/static/images/baby.gif" />
-        <style jsx>
-          {`
-            .content {
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              justify-content: center;
-              width: 100vw;
-              height: 100vh;
-            }
-          `}
-        </style>
-      </div>
-    </React.Fragment>
-  );
-};
+export default withTranslation("error")(Error);
