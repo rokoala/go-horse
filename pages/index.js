f13bcd4d0124260982f8487e5a8f44fe7c8f1b31
rokoga <rokoga@gmail.com>
2019-09-06 02:56:01 -0300

    Adsense new try

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,21 @@ import {
   ShopSection,
   ExamSection
 } from "../components/sections";
+import AdSense from "react-adsense";
 import ReactGA from "react-ga";
 import { Axiom } from "../components";
 import { withTranslation } from "react-i18next";
+const adclient = "ca-pub-1462658131006885";
 
 const Index = ({ i18n }) => {
   const refWhatIs = useRef(null);
   const refSpeed = useRef(null);
 
   useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({
-      google_ad_client: "ca-pub-1462658131006885",
-      enable_page_level_ads: true
-    });
+    // (window.adsbygoogle = window.adsbygoogle || []).push({
+    //   google_ad_client: "ca-pub-1462658131006885",
+    //   enable_page_level_ads: true
+    // });
     ReactGA.initialize("UA-137508594-1");
     ReactGA.pageview(document.location.pathname);
   }, []);
@@ -28,27 +30,38 @@ const Index = ({ i18n }) => {
       <Header refs={{ refWhatIs, refSpeed }} />
       <Head title="XGH Certification" />
       <Hero />
-      <amp-ad
-        width="100vw"
-        height="320"
-        type="adsense"
-        data-ad-client="ca-pub-1462658131006885"
-        data-ad-slot="6724162623"
-        data-auto-format="rspv"
-        data-full-width
-      >
-        <div overflow></div>
-      </amp-ad>
       <AboutSection refProp={refWhatIs} />
       <Axiom />
+      <AdSense.Google
+        client={adclient}
+        slot="3286097608"
+        style={{ display: "block" }}
+        format="auto"
+        responsive="true"
+      />
       <SpeedSection refProp={refSpeed} />
+      <AdSense.Google
+        client={adclient}
+        slot="6724162623"
+        style={{ display: "block" }}
+        format="auto"
+        responsive="true"
+      />
       <ExamSection />
-      {i18n.language === "pt" && (
+      {(i18n.language === "pt" || i18n.language === "pt-BR") && (
         <ShopSection
           imgSrc="https://http2.mlstatic.com/caneca-go-horse-certified-professional-D_NQ_NP_755061-MLB31828910724_082019-F.webp"
           link="https://produto.mercadolivre.com.br/MLB-1299690936-caneca-go-horse-certified-professional-_JM"
         />
       )}
+      <AdSense.Google
+        client={adclient}
+        className="adsbygoogle"
+        slot="3725081307"
+        style={{ display: "block" }}
+        responsive="true"
+        format="auto"
+      />
       <Footer />
     </>
   );
